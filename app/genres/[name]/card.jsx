d7ac3b4ec63c07da3155fc7edf19e335b55fe8ec
Aadarsh Adhikari <aadarshadhikar111@gmail.com>
2025-01-13 19:43:03 +0545

    modified genres page

--- app/genres/[name]/card.jsx
+++ app/genres/[name]/card.jsx
@@ -3,17 +3,14 @@ import Link from 'next/link';
 
 const Gcard = async ({ params }) => {
   let data = [];
-
   try {
     const response = await fetch(
       `https://api.themoviedb.org/3/discover/${params.type}?api_key=${process.env.NEXT_PUBLIC_API}&with_genres=${params.id}`
     );
-
     data = await response.json();
-   
     return (
       <section className="m-4">
-        <h1 className='text-2xl text-white m-2 mb-4 lg-m-6 capitalize'>{params.name} {params.type}
+        <h1 className='text-2xl text-white m-2 mb-4 lg-m-6 capitalize'>{params.name} {params.type}{params.type == 'tv' ? " shows" : ""}
         </h1>
         <div className="w-full grid grid-cols-2 lg:grid-cols-6 gap-4 text-white">
           {data?.results?.length > 0 ? (
@@ -37,15 +34,20 @@ const Gcard = async ({ params }) => {
                   />
                   <div className="py-2 sm:py-4 absolute inset-0 bottom-[-100%] hover:bottom-0 invisible group-hover:visible transition-all ease-in-out duration-500 font-semibold flex justify-center flex-col items-center">
                     <h2 className="text-sm xs:text-lg text-center mb-2">
-                      {params.type === 'movie' ? item.title : item.name}
+                      {params.type === 'movie' ? item.title : item.name }
                     </h2>
                     <div className="flex gap-1 lg:gap-3 cursor-text">
                       <p>⭐{Math.floor(item.vote_average * 10) / 10}</p>
                       <p>
-                        {params.type === 'movie'
-                          ? item.release_date.split('-')[0]
-                          : item.first_air_date.split('-')[0]}
+                      {params.type === 'movie'
+                      ? item.release_date
+                        ? item.release_date.split('-')[0]
+                        : 'N/A'
+                      : item.first_air_date
+                      ? item.first_air_date.split('-')[0]
+                      : 'N/A'}
                       </p>
+                      
                     </div>
                   </div>
                 </Link>
